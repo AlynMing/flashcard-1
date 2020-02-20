fa9982359cd6ed5f194165790d2ec9fba6abb89d
MahtaE <mahta.ellis@student.csulb.edu>
2020-02-19 21:43:38 -0800

    Fix some errors

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -17,9 +17,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ((Button) findViewById(R.id.ans1)).setTextColor(getResources().getColor(R.color.wrongAns));
+                ((Button) findViewById(R.id.ans2)).setTextColor(getResources().getColor(R.color.wrongAns));
+                ((Button) findViewById(R.id.ans3)).setTextColor(getResources().getColor(R.color.correctAns));
             }
         });
 
-        }
+        findViewById(R.id.ans2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Button) findViewById(R.id.ans1)).setTextColor(getResources().getColor(R.color.wrongAns));
+                ((Button) findViewById(R.id.ans2)).setTextColor(getResources().getColor(R.color.wrongAns));
+                ((Button) findViewById(R.id.ans3)).setTextColor(getResources().getColor(R.color.correctAns));
+            }
+        });
+        findViewById(R.id.ans3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Button) findViewById(R.id.ans1)).setTextColor(getResources().getColor(R.color.wrongAns));
+                ((Button) findViewById(R.id.ans2)).setTextColor(getResources().getColor(R.color.wrongAns));
+                ((Button) findViewById(R.id.ans3)).setTextColor(getResources().getColor(R.color.correctAns));
+            }
+        });
     }
-
+}
